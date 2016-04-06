703d84f526606c444b488e4d7c7d85ad1641fd6f
Julian Moreno Schneider <jmschnei@gmail.com>
2016-04-06 11:58:52 +0200

    Adding linguistic information package to RattlesnakeNLP

---
+++ src/main/java/de/dkt/eservices/erattlesnakenlp/linguistic/Action.java
@@ -0,0 +1,9 @@
+package de.dkt.eservices.erattlesnakenlp.linguistic;
+
+public class Action {
+
+	public String text;
+	public String uri;
+	
+	
+}
---
+++ src/main/java/de/dkt/eservices/erattlesnakenlp/linguistic/LinguisticUnit.java
@@ -0,0 +1,7 @@
+package de.dkt.eservices.erattlesnakenlp.linguistic;
+
+public interface LinguisticUnit {
+
+	public void indentedPrintToScreen(String indent);
+
+}
