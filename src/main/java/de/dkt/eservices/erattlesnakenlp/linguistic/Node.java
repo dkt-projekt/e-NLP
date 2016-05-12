417600a6bfb14ec8d8bd180a07a45a0c5d50ddcc
PeterBourgonje <bourgonje@gmail.com>
2016-05-12 13:44:41 +0200

    adding node class (which is not used currently)

---
+++ src/main/java/de/dkt/eservices/erattlesnakenlp/linguistic/Node.java
@@ -0,0 +1,23 @@
+package de.dkt.eservices.erattlesnakenlp.linguistic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+	    private List<Node> children = null;
+	    private String value;
+
+	    public Node(String value)
+	    {
+	        this.children = new ArrayList<>();
+	        this.value = value;
+	    }
+
+	    public void addChild(Node child)
+	    {
+	        children.add(child);
+	    }
+
+	
+}
