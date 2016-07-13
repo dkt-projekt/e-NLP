06533e00eaf4eb11f3e49be28d5b7ea41b32d10c
Julian Moreno Schneider <jmschnei@gmail.com>
2016-07-13 17:37:19 +0200

    .

--- src/main/java/de/dkt/eservices/eopennlp/timexRules/EnglishDateRules.java
+++ src/main/java/de/dkt/eservices/eopennlp/timexRules/EnglishDateRules.java
@@ -30,12 +30,24 @@ public class EnglishDateRules {
 		put("november", 11);
 		put("december", 12);
 	}};
+	
+	static HashMap<String, Integer> englishDayName2Integer = new HashMap<String, Integer>(){{
+		put("sunday", 1);
+		put("monday", 2);
+		put("tuesday", 3);
+		put("thursday", 4);
+		put("wednesday", 5);
+		put("friday", 6);
+		put("saturday", 7);
+	}};
 
 	
 	public static RegexNameFinder initEnglishDateFinder(){
 
 		final String monthName = "(?i)(january|february|march|april|may|june|july|august|september|october|november|december)";
 		final String monthNumber = "(0?[0-9]|1[0-2])";
+		
+		final String time = "(([0-9]|0[0-9]|1[0-2])(:|.)?([0-5][0-9])?)";
 		
 		final String dayName = "(?i)(monday|tuesday|wednesday|thursday|friday|saturday|sunday)";
 		final String dayNumber = "(0?[1-9]|1[0-9]|2[0-9]|3[0-1])";
@@ -73,6 +85,7 @@ public class EnglishDateRules {
 		englishDateRegexMap.put(16, String.format("\\b%ss\\b", yearNumber));
 		englishDateRegexMap.put(17, String.format("\\b%s\\b", dayName));
 		englishDateRegexMap.put(18, String.format("\\b%s( %s)?\\b", holidays, yearNumber));
+		englishDateRegexMap.put(19, String.format("\\b%s (?i)(a.m|p.m)\\b", time));
 		
 		Pattern[] regexes = {
 			Pattern.compile(englishDateRegexMap.get(1)),
@@ -93,6 +106,7 @@ public class EnglishDateRules {
 			Pattern.compile(englishDateRegexMap.get(16)),
 			Pattern.compile(englishDateRegexMap.get(17)),
 			Pattern.compile(englishDateRegexMap.get(18)),
+			Pattern.compile(englishDateRegexMap.get(19)),
 		};
 	
 		RegexNameFinder rnf = new RegexNameFinder(regexes, null);
@@ -479,38 +493,32 @@ public class EnglishDateRules {
 				//englishDateRegexMap.put(17, String.format("\\b%s\\b", dayName));
 				if (key == 17){
 					//TODO: fix this once I have sentence tense in place. The approach below is a simplified version, and should really be properly debugged before using it.
-					/*
-					int yearNumber = DateCommons.getYearFromAnchorDate();
-					int monthNumber = DateCommons.getMonthFromAnchorDate();
+					
 					int dayNumber = DateCommons.getDayFromAnchorDate();
-					cal.set(yearNumber, monthNumber, dayNumber);
-					if (foundDate.equalsIgnoreCase("monday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);	
+					int monthNumber = DateCommons.getMonthFromAnchorDate();
+					int yearNumber = DateCommons.getYearFromAnchorDate();
+					cal.set(yearNumber,  monthNumber, dayNumber,0,0,0);
+					int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+					int daysOfIncrease = 0;
+					int x = englishDayName2Integer.get(foundDate.trim().toLowerCase());
+					if (x > dayOfWeek){
+						// it is in the rest of the week
+						daysOfIncrease = x - dayOfWeek;
 					}
-					else if (foundDate.equalsIgnoreCase("tuesday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY);
+					else if (x == dayOfWeek){
+						// do nothing, it is the current day
+						daysOfIncrease = 0;
 					}
-					else if (foundDate.equalsIgnoreCase("wednesday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY);
+					else{ // else x < currentDayOfWeek and the day is in the next week
+						daysOfIncrease = 7 - dayOfWeek + x;
 					}
-					else if (foundDate.equalsIgnoreCase("thursday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
-					}
-					else if (foundDate.equalsIgnoreCase("friday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
-					}
-					else if (foundDate.equalsIgnoreCase("saturday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
-					}
-					else if (foundDate.equalsIgnoreCase("sunday")){
-						cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
-					}
-					normalizedStartDate = cal.getTime();
+					normalizedStartDate = DateCommons.increaseCalendar(Calendar.DATE, daysOfIncrease, cal.getTime());
 					normalizedEndDate = DateCommons.increaseCalendar(Calendar.DATE, 1, normalizedStartDate);
+					
 					dates.add(DateCommons.fullDateFormat.format(normalizedStartDate));
 					dates.add(DateCommons.fullDateFormat.format(normalizedEndDate));
 					DateCommons.updateAnchorDate(normalizedStartDate);
-					*/
+					
 				}
 				
 				//final String holidays = "(?i)(christmas( day)?|easter( monday)?|new year'?s day|may day|boxing day|independence day|labor day|memorial day|veterans day|thanksgiving)";
@@ -569,6 +577,34 @@ public class EnglishDateRules {
 					cal.set(yearNumber, monthNumber-1, dayNumber,0,0,0);
 					normalizedStartDate = cal.getTime();
 					normalizedEndDate = DateCommons.increaseCalendar(Calendar.DATE, 1, normalizedStartDate);
+					dates.add(DateCommons.fullDateFormat.format(normalizedStartDate));
+					dates.add(DateCommons.fullDateFormat.format(normalizedEndDate));
+					DateCommons.updateAnchorDate(normalizedStartDate);
+				}
+				
+				//englishDateRegexMap.put(19, String.format("\\b%s (?i)(a.m|p.m)\\b", time));
+				if (key == 19){
+
+					int dayNumber = DateCommons.getDayFromAnchorDate();
+					int monthNumber = DateCommons.getMonthFromAnchorDate();
+					int yearNumber = DateCommons.getYearFromAnchorDate();
+					int hour = 0;
+					int minute = 0;
+					String[] parts = foundDate.split("(:|\\.|\\s)");
+					if(parts[0].matches("([0-9]|0[0-9]|1[0-1])")){
+						hour = Integer.parseInt(parts[0]);
+					}
+					if(parts[1].matches("[0-5][0-9]")){
+						minute = Integer.parseInt(parts[1]);
+					}
+					if (parts[2].matches("p")){
+						hour = hour+12;
+					}else{
+						//do nothing
+					}
+					cal.set(yearNumber,  monthNumber, dayNumber, hour, minute, 0);
+					normalizedStartDate = cal.getTime();
+					normalizedEndDate = DateCommons.increaseCalendar(Calendar.MINUTE, 1, normalizedStartDate);
 					dates.add(DateCommons.fullDateFormat.format(normalizedStartDate));
 					dates.add(DateCommons.fullDateFormat.format(normalizedEndDate));
 					DateCommons.updateAnchorDate(normalizedStartDate);
