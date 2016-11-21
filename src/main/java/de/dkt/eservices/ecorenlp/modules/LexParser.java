98f0e410ef4662e34f4fe81007849b054a614fa7
webersab <sa_bine_weber@yahoo.de>
2016-11-21 11:09:04 +0100

    some updates to the coreference resolution

--- src/main/java/de/dkt/eservices/ecorenlp/modules/LexParser.java
+++ src/main/java/de/dkt/eservices/ecorenlp/modules/LexParser.java
@@ -1,6 +1,7 @@
 package de.dkt.eservices.ecorenlp.modules;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -150,7 +151,8 @@ class LexParser {
         HashMap<SpanWord,String[]> genderNumberMap= new HashMap<SpanWord,String[]>();
         LinkedHashMap<Integer,HashMap<SpanWord,String[]>> sentenceMap = new LinkedHashMap<Integer,HashMap<SpanWord,String[]>>();
         LinkedHashMap<Integer,String> sentenceMap2 = new LinkedHashMap<Integer,String>();
-        LinkedHashMap<Integer, Integer> sentenceMap3 = new LinkedHashMap<Integer,Integer>();
+        LinkedHashMap<Integer, int[]> sentenceMap3 = new LinkedHashMap<Integer,int[]>();
+        
         int sentenceCounter = 0;
         
         //do sentence splitting in for loop 
@@ -159,13 +161,18 @@ class LexParser {
         Span[] sentenceSpans = SentenceDetector.detectSentenceSpans(everything, "en-sent.bin");
         
         for (Span sentenceSpan : sentenceSpans){
+        	int[] sentenceSpans2 = new int[2];
         int sentenceStart = sentenceSpan.getStart();
         int sentenceEnd = sentenceSpan.getEnd();
        sent = everything.substring(sentenceStart, sentenceEnd);
        System.out.println("--------------------------------------------------------------------");
       System.out.println("DEBUG sent: "+sent);
       sentenceMap2.put(sentenceCounter,sent);
+      sentenceSpans2[0]=sentenceStart;
+      sentenceSpans2[1]=sentenceEnd;
+      sentenceMap3.put(sentenceCounter, sentenceSpans2);
       sentenceCounter++;
+      System.out.println("DEBUG sentenceMap3 :"+sentenceCounter+Arrays.toString(sentenceSpans2));
       //System.out.println("begins at:" + sentenceStart);
       //System.out.println("ends at:" + sentenceEnd);
      
@@ -173,8 +180,8 @@ class LexParser {
       //do parsing on the splitted sentence, this is where the magic happens
         Tree tree = parser.parse(sent);
         parser.setOptionFlags();
-       // System.out.println("DEBUG tree: ");
-       // tree.pennPrint();
+       System.out.println("DEBUG tree: ");
+       tree.pennPrint();
         
         //Extract NPs and PPERs and adds them to list nps
         Object[] a = tree.toArray();
@@ -258,7 +265,19 @@ class LexParser {
         	List<String> list2 = Arrays.asList(word.split(" "));
         	List<String> wordTags = new ArrayList<String>();
       	  	int numberOfWords2 = list2.size();
-        	String line32 = Files.readAllLines(Paths.get("C:\\Users\\Sabine\\Desktop\\WörkWörk\\15.txt")).get(wordNumber-1);
+      	  	String line32 = Files.readAllLines(Paths.get("C:\\Users\\Sabine\\Desktop\\WörkWörk\\15.txt")).get(wordNumber-1);
+      	  	
+      	  	//cleanup of the result of the RFTagger
+      	  	/*FileWriter fw = new FileWriter("C:\\Users\\Sabine\\Desktop\\WörkWörk\\16.txt"); 
+        	for(int i=0; i<numberOfWords2; i++){
+        		if (!line32.equals("")) // don't write out blank lines
+        	    {
+        	        fw.write(line32, 0, line32.length());
+        	    }
+        	}
+        	fw.close();*/
+        	
+        	
         	for(int i=0; i<numberOfWords2; i++){
         		line32 = Files.readAllLines(Paths.get("C:\\Users\\Sabine\\Desktop\\WörkWörk\\15.txt")).get(wordNumber-1+i);
         		System.out.println("DEBUG wordTag: "+line32);
@@ -315,32 +334,54 @@ class LexParser {
         sentenceMap.put(sentenceCounter, genderNumberMap);
         
         }
+      
         
-        for( Entry<Integer, String> a :sentenceMap2.entrySet()){
-        	String sent1 = a.getValue();
+        // I put a map of the number of the sentence and the sentence indexes in here
+        for( Entry<Integer, int[]> a :sentenceMap3.entrySet()){
+        	int[] indexes= a.getValue();
         	int key = a.getKey();
-        	System.out.println("LEVEL 1: sent1 "+sent1);
+        	//System.out.println("LEVEL 1: sent1 "+key+Arrays.toString(indexes));
+        	
         	//get the nps from the sentences and put them in mnps
-        	ArrayList<ArrayList<String>> mnps = new ArrayList<ArrayList<String>>();
-        	ArrayList<ArrayList<String>> nps = stringToNpList(sent1);
-        	for (ArrayList<String> ksdfgh : nps){
-        		mnps.add(ksdfgh);
-        	}
-        	//loop per sentence, get nps for next sentence and put them in mnps
-        	for (int j = key+1; j < Math.min(sentenceMap2.size(), key+5); j++){
-        		String sent2 = sentenceMap2.get(j);
-        		System.out.println("LEVEL 2: sent2" + sent2);
-        		for (ArrayList<String> abc : stringToNpList(sent2)){
+        	
+        	ArrayList<SpanWord> mnps = stringToNpListwIndex(indexes);
+        	
+        	//loop per next 5 sentences, get nps for next sentence and put them in mnps
+        	for (int j = key+1; j < Math.min(sentenceMap3.size(), key+5); j++){
+        		int[] indexes2 = sentenceMap3.get(j);
+        		//System.out.println("LEVEL 2: sent2" + Arrays.toString(indexes2));
+        		for (SpanWord abc : stringToNpListwIndex(indexes2)){
         			mnps.add(abc);
         		}
+        	}
+        	for(SpanWord t : mnps){
+        		System.out.println("DEBUG mnps: "+t.getText()+" "+t.getStartSpan());
         	}
         	// loop through mnps and compare the single items
         	for (int k = 0; k < mnps.size(); k++){
         		for (int l = k+1; l < mnps.size(); l++){
-        			System.out.println("np1:"+mnps.get(k));
-        			System.out.println("np2:"+mnps.get(l));
-        			if (compareLists(mnps.get(k),mnps.get(l))){
-        				System.out.println("FOUND MATCHING ITEMS:"+"ITEMS GO HERE");
+        			SpanWord r = mnps.get(k);
+        			SpanWord s = mnps.get(l);
+        			//System.out.println("np1:"+mnps.get(k));
+        			
+        			if (compareListsSpan(mnps.get(k),mnps.get(l))){
+        				
+        				//System.out.println("FOUND MATCHING ITEMS:" + r.getText() + '\t'+s.getText());
+        				String sent1 = everything.substring(indexes[0], indexes[1]);
+        				String[] context = new String[5];
+        				int g = 0;
+        				for (int h = key+1; h < Math.min(key+5, sentenceMap2.size()); h++){
+        					context[g] = everything.substring(sentenceMap3.get(h)[0],sentenceMap3.get(h)[1]); 
+        					g++;
+        				}
+        				//System.out.println("INFO:                  Found matching items:" + r.getText()+" "+r.getStartSpan()+ "\t"+s.getText()+" "+s.getStartSpan());
+        				String contextAsString = sent1;
+        				for (String ssss : context){
+        					contextAsString += "\n" + ssss;
+        				}
+        				//System.out.println("in context:" + contextAsString);
+        				System.out.println("--------------------------------------------------------------------------------");
+        				
         			}
         			//if edit distance is smaller than X, also print/consider as referring to the same thing
         		}
@@ -348,29 +389,7 @@ class LexParser {
         	
         }
         
-        //Sliding window walk trough sentences and match
-       /* int i = 0;
-        int windowSize = 3;
-        
-        
-        for (int x=0; x<sentenceMap.size(); x++){
-        	List<SpanWord> npsInWindow = new ArrayList<SpanWord>();
-        	
-        	for(int y=0; y<windowSize; y++){
-        	
-        		HashMap<SpanWord,String[]> bla = sentenceMap.get(x+y);
-        		List<SpanWord> npsInSentence = new ArrayList<SpanWord>();
-        		
-        		for(Entry<SpanWord, String[]> entry : genderNumberMap.entrySet()){
-        			
-        			SpanWord key = entry.getKey();
-        			npsInSentence.add(key);
-        			System.out.println(key.getText());
-        		}
-        		npsInWindow.addAll(npsInSentence);
-        			
-        		}
-        	}*/
+   
     }
     
     public static boolean compareLists(ArrayList<String> list1, ArrayList<String> list2){
@@ -384,9 +403,70 @@ class LexParser {
 		}
     	return true;
     }
+    
+    // recognizes exact match, nearly exact match if only one word, differences in case and articles if two words
+    public static boolean compareListsSpan(SpanWord s1, SpanWord s2){
+    	
+    	String w1 = s1.getText();
+    	String w2 = s2.getText();
+    	boolean retValue = false;
+    	if (w1.equalsIgnoreCase(w2)){
+    		retValue = true;
+    	}
+
+    	else if (!w1.contains(" ") && !w2.contains(" ")&&w1.length() > 4 && w2.length()> 4){
+    		String naiveStemW1 = (w1.substring(0, (Integer)w1.length() - w1.length()/5)).toLowerCase();
+    		String naiveStemW2 = (w2.substring(0, (Integer)w2.length() - w2.length()/5)).toLowerCase();
+    		if(naiveStemW1.contains(naiveStemW2) || naiveStemW2.contains(naiveStemW1)){
+    			System.out.println("DEBUGGING STEMMED APPROACH:" + naiveStemW1 + "\t" + naiveStemW2);
+    			retValue = true;
+    		}
+    	}
+    	else if ((w1.split(" ").length==2 && w1.matches("(?i)(.*)\\b(der|die|das|dem|den|des|ein|eine|einen|einem|eines|einer)\\b(.*)"))){
+    		String [] w1AsArray = w1.split(" ");
+    		
+    		w1 = w1AsArray[1];
+    		
+    		if (w1.length() > 4 && w2.length()> 4){
+        		String naiveStemW1 = (w1.substring(0, (Integer)w1.length() - w1.length()/5)).toLowerCase();
+        		String naiveStemW2 = (w2.substring(0, (Integer)w2.length() - w2.length()/5)).toLowerCase();
+        		if(naiveStemW1.contains(naiveStemW2) || naiveStemW2.contains(naiveStemW1)){
+        			System.out.println("DEBUGGING STEMMED APPROACH2a:" + naiveStemW1 + "\t" + naiveStemW2);
+        			retValue = true;
+        		}
+    	} }
+    	else if ((w2.split(" ").length==2 && w2.matches("(?i)(.*)\\b(der|die|das|dem|den|des|ein|eine|einen|einem|eines|einer)\\b(.*)"))){
+    		
+    		String [] w2AsArray = w2.split(" ");
+    		
+    		w2 = w2AsArray[1];
+    		if (w1.length() > 4 && w2.length()> 4){
+        		String naiveStemW1 = (w1.substring(0, (Integer)w1.length() - w1.length()/5)).toLowerCase();
+        		String naiveStemW2 = (w2.substring(0, (Integer)w2.length() - w2.length()/5)).toLowerCase();
+        		if(naiveStemW1.contains(naiveStemW2) || naiveStemW2.contains(naiveStemW1)){
+        			System.out.println("DEBUGGING STEMMED APPROACH2b:" + naiveStemW1 + "\t" + naiveStemW2);
+        			retValue = true;
+        		}
+    	} }
+    	else if ((w1.split(" ").length==2 && w2.split(" ").length==2 && w2.matches("(?i)(.*)\\b(der|die|das|dem|den|des|ein|eine|einen|einem|eines|einer)\\b(.*)")&& w1.matches("(?i)(.*)\\b(der|die|das|dem|den|des|ein|eine|einen|einem|eines|einer)\\b(.*)"))){
+    		String [] w1AsArray = w1.split(" ");
+    		String [] w2AsArray = w2.split(" ");
+    		w1 = w1AsArray[1];
+    		w2 = w2AsArray[1];
+    		if (w1.length() > 4 && w2.length()> 4){
+        		String naiveStemW1 = (w1.substring(0, (Integer)w1.length() - w1.length()/5)).toLowerCase();
+        		String naiveStemW2 = (w2.substring(0, (Integer)w2.length() - w2.length()/5)).toLowerCase();
+        		if(naiveStemW1.contains(naiveStemW2) || naiveStemW2.contains(naiveStemW1)){
+        			System.out.println("DEBUGGING STEMMED APPROACH2c:" + naiveStemW1 + "\t" + naiveStemW2);
+        			retValue = true;
+        		}
+    	} }
+    	
+    	return retValue;
+    }
         
         
-    // do it with indexes only, not with the texts, okay?
+    // do it with indexes only, not with the texts
     
     public static  ArrayList<ArrayList<String>> stringToNpList (String sent){
     Tree tree = parser.parse(sent);
@@ -415,5 +495,107 @@ class LexParser {
     	
     
 	return nps;}
-                          
+    
+    
+    //gets the sentence indexes, returns the NPs with indexes
+    public static  ArrayList<SpanWord> stringToNpListwIndex(int[] indexes) throws IOException{
+    	ArrayList<SpanWord> bla = new ArrayList<SpanWord>();
+    	String everything = new String();
+        
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\Sabine\\Desktop\\WörkWörk\\14cleaned.txt");
+        try {
+            everything = IOUtils.toString(inputStream);
+           
+        } finally {
+            inputStream.close();
+        }
+        
+        
+        	String sent = everything.substring(indexes[0], indexes[1]);
+        
+        	Tree tree = parser.parse(sent);
+        	parser.setOptionFlags();
+        	// System.out.println("DEBUG tree: ");
+        	// tree.pennPrint();
+        
+        	//Extract NPs and PPERs and adds them to list nps
+        	Object[] a = tree.toArray();
+        	ArrayList<ArrayList<String>> nps = new ArrayList<ArrayList<String>>();
+        	for (Object s : a){
+        		LabeledScoredTreeNode t = (LabeledScoredTreeNode)s;
+        		if (t.label().toString().equalsIgnoreCase("np")||t.label().toString().equalsIgnoreCase("pper")){
+        			//if (t.label().toString().equalsIgnoreCase("np")){
+        			ArrayList<String> npAsList = new ArrayList<String>();
+        			for (Tree it : t.flatten()){
+        				if ((it.isLeaf())){
+        					npAsList.add(it.pennString().trim());
+        				}
+        			}
+        		
+        			nps.add(npAsList);
+        		}
+        	}
+        	
+        	int counter = 0;
+            
+            ArrayList<SpanWord> wordSpans = new ArrayList<SpanWord>();
+           
+            for (ArrayList<String> subl : nps){
+            	//System.out.println("DEBUG subl: "+subl); 
+            	String word = new String(); 
+            	word =(subl.get(0));
+            	for(int x=1; x<subl.size(); x++){
+            		word = word + " " +(subl.get(x));
+            	    
+            	}
+        
+        
+        	
+            List<Integer> pos = new ArrayList<Integer>();
+        	
+      	  if (sent.toLowerCase().contains(word.toLowerCase()) && sent.toLowerCase().indexOf(word.toLowerCase()) != sent.toLowerCase().lastIndexOf(word.toLowerCase())){
+      		    Matcher m = Pattern.compile("(?i)\\b"+word+"\\b").matcher(sent);
+      		    counter++;
+      		    while (m.find())
+      		    {
+      		        pos.add(m.start());
+      		    
+      		    }
+      		    
+      		    }
+      	
+      	
+      	SpanWord d = new SpanWord("",0,0);
+      	
+      	if(pos.size()>1){
+      		
+      		int begin = indexes[0] + pos.get(counter-1);
+          	int end = begin +word.length();
+      		d = new SpanWord(word,begin,end);
+      		wordSpans.add(d);
+      		//System.out.println("DEBUG word span: "+d.getText()+" "+d.getStartSpan()+" "+d.getEndSpan());
+      		
+      	}else{
+      		String inputStr = sent;
+      	    String patternStr = word;
+      	    Pattern pattern = Pattern.compile("\\b"+patternStr+"\\b");
+      	    Matcher matcher = pattern.matcher(inputStr);
+      	    if(matcher.find()){
+      	    	int begin = indexes[0] + matcher.start();
+      	    	int end = begin +word.length();
+      	    	d = new SpanWord(word,begin,end);
+      	    	wordSpans.add(d);}
+      	    else{
+      	    	System.out.println("Something went wrong with the matching!");
+      	    	}
+      		//System.out.println("DEBUG word span: "+d.getText()+" "+d.getStartSpan()+" "+d.getEndSpan());
+      	}
+      	
+    	}
+//            bla.add(wordSpans);
+//		return bla;
+            return wordSpans;
+                       
     }
+    
+}
